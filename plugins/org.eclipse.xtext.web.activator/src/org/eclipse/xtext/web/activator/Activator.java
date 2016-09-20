d6513e35d886c1f6822023d4a41b1a2d49d42d3a
DZ <you@example.com>
2016-09-20 12:27:27 +0300

    Настроен запуск статики из org.eclipse.xtext.web плагином org.eclipse.xtext.web.activator

--- plugins/org.eclipse.xtext.web.activator/src/org/eclipse/xtext/web/activator/Activator.java
+++ plugins/org.eclipse.xtext.web.activator/src/org/eclipse/xtext/web/activator/Activator.java
@@ -6,11 +6,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.Hashtable;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.jetty.server.handler.ContextHandler;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 public class Activator implements BundleActivator {
 
@@ -43,7 +53,49 @@ public class Activator implements BundleActivator {
 //		} catch (IOException e) {
 //		    e.printStackTrace();
 //		}
-	    context.getBundle().getSymbolicName();
+//		
+//		
+////		URL entry = context.getBundle().getEntry(".");
+////		if (entry != null)
+////		{
+////		URLConnection connection = entry.openConnection();
+////		if (connection instanceof BundleURLConnection)
+////		{
+////		URL fileURL = ((BundleURLConnection) connection).getFileURL();
+////		URI uri = new URI(fileURL.toString());
+////		String path = new File(uri).getAbsolutePath();
+////		System.out.printf("This is the path: %s\n", path); 
+////		}
+////		}
+//		
+//		// упрощенный пример 
+//        File f = new File("/Users/admin/Work/1C/workspace/xtext-web/plugins/org.eclipse.xtext.web.activator/handlers/cssHandler.xml");
+//        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+//        DocumentBuilder builder = factory.newDocumentBuilder();
+//        Document document = builder.parse(f);
+//        Element root = document.getDocumentElement();
+//        // для простоты сразу берем message
+//        Element message = (Element) root.getElementsByTagName("Arg").item(0);
+//        message.setTextContent("lol"); 
+//        String textContent = message.getTextContent(); // тоже для упрощения
+//        System.err.println(textContent);
+//        
+//		
+//		Bundle bundle = Platform.getBundle("org.eclipse.xtext.web.activator");
+//		Path path = new Path("css/xtext/");
+////		URL fileURL = Platform.find(bundle, path);
+//		
+//		URL fileURL = FileLocator.find(bundle, path, null);
+//		System.err.println(Platform.resolve(fileURL) + " " 
+//				+ context.getBundle().getSymbolicName());
+////		InputStream in = fileURL.openStream();
+//		 
+//		
+//
+//		
+//		
+//		
+//	    context.getBundle().getSymbolicName();
 		
 		ContextHandler ch1= new ContextHandler();
 		Hashtable<String, String> props1 = new Hashtable<>();
